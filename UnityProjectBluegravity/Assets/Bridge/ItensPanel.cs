ea860e26540fbe9e6d4167fc429c59a32c5f4dae
Nascimento Lucas <devlucasnascimento@gmail.com>
2023-10-28 23:46:29 -0300

    Implementing store buttons

--- UnityProjectBluegravity/Assets/Bridge/ItensPanel.cs
+++ UnityProjectBluegravity/Assets/Bridge/ItensPanel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 namespace Bluegravity.Game.Item
 {
@@ -13,11 +14,14 @@ namespace Bluegravity.Game.Item
         private ItemUICell _cellPrefab;
         [SerializeField]
         private LayoutGroup _layout;
+        [SerializeField]
+        private ScrollRect _scroll;
 
         public void CreateItem(IHandleItem handle, IViewItem view)
         {
             ItemUICell cell = Instantiate(_cellPrefab, _layout.transform);
             cell.Setup(handle, view);
+            _scroll.verticalNormalizedPosition = 1;
         }
     }
 
